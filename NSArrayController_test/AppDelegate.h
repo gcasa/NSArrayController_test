f2150f849b5096b81b9c11943517149bb8a73b4c
Gregory Casamento <greg.casamento@gmail.com>
2020-10-06 00:34:07 -0400

    Initial Commit

---
+++ NSArrayController_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSArrayController_test
+//
+//  Created by Gregory Casamento on 10/6/20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
